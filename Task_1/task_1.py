a1e6d2c181ce678e174dbc4877137fc8253d025f
VexheslavSP <willy0@rambler.ru>
2022-12-16 17:26:18 +0300

    выполнено 1 задание

---
+++ Task_1/task_1.py
@@ -0,0 +1,17 @@
+import re
+while (True):
+    number_str = input("Введите трехзначное число ")
+    try:
+        number = int(number_str)
+        if (100 <= abs(number) <= 1000):
+            tmp_num = abs(number)
+            break
+        else:
+            raise
+    except:
+        print("Неккоректный ввод числа")
+result = 0
+while (tmp_num > 0):
+    result += tmp_num % 10
+    tmp_num //= 10
+print(result)
